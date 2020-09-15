2b098626fd9ef3931d779cd24f172ece720e9251
Ahmed Mustafa <ahmedmustafa5832@gmail.com>
2020-09-15 21:02:37 +0500

    initial commit

---
+++ lib/screens/home/components/body.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+//import '../../../size_config.dart';
+import 'categories.dart';
+import 'discount_banner.dart';
+import 'home_header.dart';
+import 'popular_product.dart';
+import 'special_offers.dart';
+import 'package:shop_app/size_config.dart';
+
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height: getProportionateScreenHeight(20.0)),
+            HomeHeader(),
+            SizedBox(height: getProportionateScreenWidth(10.0)),
+            DiscountBanner(),
+            Categories(),
+            SpecialOffers(),
+            SizedBox(height: getProportionateScreenWidth(30.0)),
+            PopularProducts(),
+            SizedBox(height: getProportionateScreenWidth(30.0)),
+          ],
+        ),
+      ),
+    );
+  }
+}
